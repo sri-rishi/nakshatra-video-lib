bfdaee2c25441b470ad731e6e9f511f6185cd787
sri-rishi <sririshi2612@gmail.com>
2022-04-05 18:57:26 +0530

    chore: adde data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,123 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
-  },
-  {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-  },
+    _id: "LxO-6rlihSg",
+    title: "Learn Photography [Full Course] by Australian Geographic Photographer Chris Bray",
+    creator: "Chris Bray Photography",
+    categoryName: "Learning",
+    views: 2326315,
+    postedBefore: "1 year ago"
+    },
+    {
+    _id: "gmVo_YLUtbs",
+    title: "POV Street Photography Vlog on Indian Streets w/ Canon R5 + Sigma 135mm - NSB Pictures",
+    creator: "NSB Pictures",
+    categoryName: "Travelling",
+    views: 9490866,
+    postedBefore: "3 months ago"
+    },
+    {
+    _id: "WXdAX0No2hM",
+    title: "Be a Better Photographer in 45 mins",
+    creator: "Sudhir Shivaram Photography",
+    categoryName: "Learning",
+    views: 7192931,
+    postedBefore: "4 years ago"
+    },
+    {
+    _id: "IyR_uYsRdPs",
+    title: "Photoshop for Beginners | FREE COURSE",
+    creator: "Envato Tuts+",
+    categoryName: "Learning",
+    views: 9490866,
+    postedBefore: "2 years ago"
+    },
+    {
+    _id: "XzwVlTrIlgc",
+    title: "BANARAS DEKHIYE HUMARE STYLE MEIN || Varanasi TRAVEL VLOG",
+    creator: "camerawalebhaiya",
+    categoryName: "Travelling",
+    views: 316415,
+    postedBefore: "1 year ago"
+    },
+    {
+    _id: "cwJUeez1FG0",
+    title: "Prayagraj Vlog. New Begginings. Allahabad Vlog",
+    creator: "Bhupendra Meena",
+    categoryName: "Travelling",
+    views: 2571,
+    postedBefore: "2 months ago"
+    },
+    {
+    _id: "hWr2e0vQaLA",
+    title: "Kedarnath Yatra 2020 | Kedarnath Vlog | Travel During COVID-19 | Complete Guide for Kedarnath Dham",
+    creator: "TheBlueSpoon Traveller",
+    categoryName: "Travelling",
+    views: 1500787,
+    postedBefore: "1 year ago"
+    },
+    {
+    _id: "rP7KOBCPQBc",
+    title: "3 Lenses EVERY Photographer NEEDS & Why!",
+    creator: "sawyerhartman",
+    categoryName: "Technical",
+    views: 674532,
+    postedBefore: "2 years ago"
+    },
+    {
+    _id: "Y9CuwfUJ5rk",
+    title: "Best Photography Cameras for EVERY Budget in 2021 | $500 - $5,000",
+    creator: "Dan Watson",
+    categoryName: "Technical",
+    views: 67939,
+    postedBefore: "7 months ago"
+    },
+    {
+    _id: "ciIDmd7gPZ8",
+    title: "Buying Your First Camera? 6 Things To Consider When Choosing Photography Gear",
+    creator: "Allison Anderson",
+    categoryName: "Technical",
+    views: 49607,
+    postedBefore: "1 year ago"
+    },
+    {
+    _id: "rnciy0hqTUc",
+    title: "5 Ways To Start Your Photography Career",
+    creator: "Alex Iby",
+    categoryName: "Career",
+    views: 12223,
+    postedBefore: "3 years ago"
+    },
+    {
+    _id: "TCKEFwaUNcA",
+    title: "Photography Business & Career Plan with Earnings in India 2021 (Tips Hindi)",
+    creator: "Praveen Bhat Photography",
+    categoryName: "Career",
+    views: 5335,
+    postedBefore: "8 months ago"
+    },
+    {
+    _id: "R7i1NcvY_do",
+    title: "All you need to know for starting Wildlife Photography? | Monetizing Wildlife Photography",
+    creator: "Aarzoo Khurana",
+    categoryName: "Wildlife",
+    views: 36781,
+    postedBefore: "10 months ago"
+    },
+    {
+    _id: "ugTThtin5Ng",
+    title: "Safari of my Life - Wildlife Photography Documentary with Klaus Tiedge",
+    creator: "Klaus Tiedge Fine Art Photography",
+    categoryName: "Wildlife",
+    views: 1775364,
+    postedBefore: "1 year ago"
+    },
+    {
+    _id: "NRPLSwqgHQw",
+    title: "RED DEER - WILDLIFE PHOTOGRAPHY - behind the scenes vlog with wildlife photographer Morten Hilmer",
+    creator: "Morten Hilmer",
+    categoryName: "Wildlife",
+    views: 1757553,
+    postedBefore: "4 years ago"
+    },
 ];
