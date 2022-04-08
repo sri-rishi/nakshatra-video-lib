bfcffbb13f936a62158473dd648d5c04fee824ac
sri-rishi <sririshi2612@gmail.com>
2022-04-08 12:36:18 +0530

    feat: added active style

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,32 +1,38 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AiOutlineHome, AiOutlineLike, MdOutlineWatchLater, MdPlaylistPlay, RiHistoryFill} from "../../assets";
 
 export const Sidebar = () => {
+
+    const getActiveStyle = ({isActive}) => ({
+        background: isActive && "var(--primary-color)",
+        color: isActive && "var(--white-color)"
+    })
+
     return (
         <aside className="aside-box">
             <ul className="aside-items-box flex-column gap-1">
-                <Link to="/">
+                <NavLink to="/" style={getActiveStyle}>
                     <li className="aside-items flex-row gap-1">
                         <AiOutlineHome className="icon-vr-align mt-4-px"/> 
                         <span className="noto-fonts">Home</span>
                     </li>
-                </Link>
-                <Link to="/user/watchlater" >
+                </NavLink>
+                <NavLink to="/user/watchlater" style={getActiveStyle}>
                     <li className="aside-items flex-row gap-1">
                         <MdOutlineWatchLater className="icon-vr-align mt-4-px"/> 
                         <span className="noto-fonts">Watch Later</span>
                     </li>
-                </Link>
+                </NavLink>
                 <li className="aside-items flex-row gap-1">
                     <MdPlaylistPlay className="icon-vr-align mt-4-px"/>
                     <span className="noto-fonts">Playlists</span>
                 </li>
-                <Link to="/user/likes">
+                <NavLink to="/user/likes" style={getActiveStyle}>
                     <li className="aside-items flex-row gap-1">
                         <AiOutlineLike className="icon-vr-align mt-4-px"/>
                         <span className="noto-fonts">Liked videos</span>
                     </li>
-                </Link>
+                </NavLink>
                 <li className="aside-items flex-row gap-1">
                     <RiHistoryFill className="icon-vr-align mt-4-px"/>
                     <span className="noto-fonts">History</span>
