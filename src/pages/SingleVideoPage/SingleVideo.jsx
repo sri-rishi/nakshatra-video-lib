493232fa24ece7e5fca74e626541d9b3daec1cd4
sri-rishi <sririshi2612@gmail.com>
2022-04-10 23:54:48 +0530

    feat: added service functionality

--- src/pages/SingleVideoPage/SingleVideo.jsx
+++ src/pages/SingleVideoPage/SingleVideo.jsx
@@ -1,24 +1,21 @@
 import { useParams } from "react-router-dom"
 import YouTube from "react-youtube"
 import { useEffect, useState } from "react";
-import { getVideoById, postVideoToHistory } from "../../ApiCalls";
+import { getVideoById} from "../../ApiCalls";
 import {Button, Navbar, Sidebar} from "../../Components/index";
+import { historyHandler , addToLikedVideos, removeFromLikedVideo, findItemInArray, watchLaterHandler, calculateView} from "../../Helper";
 import {AiOutlineDislike, AiOutlineLike, MdOutlineWatchLater, MdPlaylistAdd } from "../../assets";
 import { useServiceData } from "../../Context";
 
 export const SingleVideo = () => {
     const [video, setVideo] = useState();
     const {videoId} =  useParams();
-    const {serviceListDispatch} = useServiceData()
+    const {serviceListDispatch, likedVideoList, watchLaterList} = useServiceData()
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         getVideoById(videoId, setLoading, setVideo)
     }, [videoId]);
-
-    const historyHandler = (video) => {
-        postVideoToHistory(video, serviceListDispatch)
-    }
 
     if(loading) {
         return <h1>Loading ......</h1>
@@ -31,7 +28,12 @@ export const SingleVideo = () => {
             <main className="main-box flex-column">
                 <div className="video-box flex-column gap-2">
                     <div className="single-video-box">
-                        <YouTube videoId={video?._id} containerClassName="single-video" className="youtube-video" onPlay={() => historyHandler(video)}/>
+                        <YouTube 
+                            videoId={video?._id} 
+                            containerClassName="single-video" 
+                            className="youtube-video" 
+                            onPlay={() => historyHandler(video, serviceListDispatch)}
+                        />
                     </div>
                     <div className="flex-column gap-1">
                         <div className="video-title-box flex-column gap-8-px">
@@ -40,22 +42,25 @@ export const SingleVideo = () => {
                         </div>
                         <div className="flex-row align-center justify-between video-action-box border-bottom">
                             <p className="flex-row align-center gap-8-px">
-                                {video?.views} views 
+                                {calculateView(video?.views)} views 
                                 <span className="point-div mt-3-px"></span> {video?.postedBefore}
                             </p>
                             <div className="flex-row align-center gap-1">
                                 <Button 
-                                    className={`btn-border-none bg-transparent video-cta-btn icon-size `} 
+                                    className={`btn-border-none bg-transparent video-cta-btn icon-size ${findItemInArray(video?._id, likedVideoList) && "color-dark-primary"}`} 
                                     icon={<AiOutlineLike className="icon-vr-align mr-1"/>}
+                                    onClick={() => !findItemInArray(video?._id, likedVideoList) && addToLikedVideos(video, serviceListDispatch)}
                                 />
                                 <Button 
                                     className={`btn-border-none bg-transparent video-cta-btn icon-size `} 
                                     icon={<AiOutlineDislike className="icon-vr-align mr-1"/>}
+                                    onClick={() => findItemInArray(video?._id, likedVideoList) && removeFromLikedVideo(video?._id, serviceListDispatch)}
                                 />
                                 <Button 
                                     className={`btn-border-none bg-transparent video-cta-btn`} 
                                     icon={<MdOutlineWatchLater className="icon-vr-align mr-8-px icon-size"/>} 
-                                    text={`Add to watch later`} 
+                                    text={findItemInArray(video?._id, watchLaterList) ? "Remove from Watch Later" : "Add to Watch Later"} 
+                                    onClick={() => watchLaterHandler(video?._id, video, watchLaterList, serviceListDispatch)}
                                 />
                                 <Button 
                                     className={`btn-border-none bg-transparent video-cta-btn`} 
