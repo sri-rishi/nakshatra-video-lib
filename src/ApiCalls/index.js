38e9f523b853d26933b91eaf70806208018f083e
sri-rishi <sririshi2612@gmail.com>
2022-04-10 18:56:51 +0530

    refactor: refactored code

--- src/ApiCalls/index.js
+++ src/ApiCalls/index.js
@@ -1,5 +1,5 @@
-import { getData, getCategoryData, getDataFromWatchLater, getDataFromLikedVideo } from "./getMethodRequest";
-import {loginHandler, signInHandler, postVideoToWatchLater, postVideoToLikedVideo} from "./postMethodRequest";
+import { getData, getCategoryData, getDataFromWatchLater, getDataFromLikedVideo, getVideoById, getDataFromHistory } from "./getMethodRequest";
+import {loginHandler, signInHandler, postVideoToWatchLater, postVideoToLikedVideo, postVideoToHistory} from "./postMethodRequest";
 import { deleteVideoFromWatchLater, deleteVideoFromLikedVideo } from "./deleteMethodRequest";
 
-export { getData, getCategoryData, loginHandler, signInHandler, postVideoToWatchLater, getDataFromWatchLater, deleteVideoFromWatchLater, postVideoToLikedVideo, deleteVideoFromLikedVideo, getDataFromLikedVideo}
+export { getData, getCategoryData, loginHandler, signInHandler, postVideoToWatchLater, getDataFromWatchLater, deleteVideoFromWatchLater, postVideoToLikedVideo, deleteVideoFromLikedVideo, getDataFromLikedVideo, getVideoById, postVideoToHistory, getDataFromHistory}
