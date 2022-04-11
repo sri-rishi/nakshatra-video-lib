98878a7b4494d459700c3e0c037fd67fe3f73238
sri-rishi <sririshi2612@gmail.com>
2022-04-11 18:29:25 +0530

    style: adde icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,10 +1,10 @@
-import {FaUserCircle, FaSearch} from "react-icons/fa";
+import {FaUserCircle, FaSearch, FaTimes} from "react-icons/fa";
 import {GiHamburgerMenu} from "react-icons/gi";
 import { BsThreeDotsVertical} from "react-icons/bs";
 import {ImLinkedin} from "react-icons/im";
 import {MdPlaylistAdd, MdOutlineWatchLater, MdPlaylistPlay} from "react-icons/md";
 import {RiHistoryFill, RiDeleteBin5Fill} from "react-icons/ri";
-import {AiOutlineHome, AiOutlineLike, AiFillLike, AiOutlineDislike} from "react-icons/ai"
+import {AiOutlineHome, AiOutlineLike, AiFillLike, AiOutlineDislike, AiOutlinePlus} from "react-icons/ai"
 
 
-export {FaUserCircle, GiHamburgerMenu, FaSearch, ImLinkedin, BsThreeDotsVertical, MdPlaylistAdd, MdOutlineWatchLater, RiHistoryFill, AiOutlineHome, AiOutlineLike, MdPlaylistPlay, AiFillLike, AiOutlineDislike, RiDeleteBin5Fill};
+export {FaUserCircle, GiHamburgerMenu, FaSearch, ImLinkedin, BsThreeDotsVertical, MdPlaylistAdd, MdOutlineWatchLater, RiHistoryFill, AiOutlineHome, AiOutlineLike, MdPlaylistPlay, AiFillLike, AiOutlineDislike, RiDeleteBin5Fill, FaTimes, AiOutlinePlus};
