03a64561c1a0d03313165287ace8b2c28200f003
sri-rishi <sririshi2612@gmail.com>
2022-04-12 12:15:02 +0530

    chore: imported icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,10 +1,11 @@
-import {FaUserCircle, FaSearch, FaTimes} from "react-icons/fa";
+import {FaUserCircle, FaSearch} from "react-icons/fa";
 import {GiHamburgerMenu} from "react-icons/gi";
 import { BsThreeDotsVertical} from "react-icons/bs";
 import {ImLinkedin} from "react-icons/im";
 import {MdPlaylistAdd, MdOutlineWatchLater, MdPlaylistPlay} from "react-icons/md";
 import {RiHistoryFill, RiDeleteBin5Fill} from "react-icons/ri";
-import {AiOutlineHome, AiOutlineLike, AiFillLike, AiOutlineDislike, AiOutlinePlus} from "react-icons/ai"
+import {AiOutlineHome, AiOutlineLike, AiFillLike, AiOutlineDislike, AiOutlinePlus} from "react-icons/ai";
+import {VscClose} from "react-icons/vsc";
 
 
-export {FaUserCircle, GiHamburgerMenu, FaSearch, ImLinkedin, BsThreeDotsVertical, MdPlaylistAdd, MdOutlineWatchLater, RiHistoryFill, AiOutlineHome, AiOutlineLike, MdPlaylistPlay, AiFillLike, AiOutlineDislike, RiDeleteBin5Fill, FaTimes, AiOutlinePlus};
+export {FaUserCircle, GiHamburgerMenu, FaSearch, ImLinkedin, BsThreeDotsVertical, MdPlaylistAdd, MdOutlineWatchLater, RiHistoryFill, AiOutlineHome, AiOutlineLike, MdPlaylistPlay, AiFillLike, AiOutlineDislike, RiDeleteBin5Fill, VscClose, AiOutlinePlus};
