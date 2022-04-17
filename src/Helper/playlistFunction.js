a697b92382b9dc55158cf6e3cb70e80f130bd9cd
sri-rishi <sririshi2612@gmail.com>
2022-04-17 23:10:31 +0530

    chore: updated files

--- src/Helper/playlistFunction.js
+++ src/Helper/playlistFunction.js
@@ -1,26 +1,25 @@
 import { postPlaylist, deletePlaylistFromPlaylists, postVideoInPlaylist, deleteVideoFromPlaylist } from "../ApiCalls";
-import { findItemInArray } from "./commonFunction";
 
 const newPlaylistHandler = (newPlaylistName, serviceListDispatch, allPlaylistArray, setShowCreateNewModal, setNewPlaylistName) => {
     const title = allPlaylistArray && allPlaylistArray.some(playlistObj => playlistObj.title === newPlaylistName)
     setShowCreateNewModal(false)
     if(!title) {
-        postPlaylist(newPlaylistName, serviceListDispatch);
+        postPlaylist(newPlaylistName, serviceListDispatch, setToastData);
     }else {
         alert("Playlist Name has been taken")
     }
     setNewPlaylistName("");
 }
 
-const removePlaylistHandler = (playlistId, serviceListDispatch) => {
-    deletePlaylistFromPlaylists(playlistId, serviceListDispatch);
+const removePlaylistHandler = (playlistId, serviceListDispatch, setToastData) => {
+    deletePlaylistFromPlaylists(playlistId, serviceListDispatch, setToastData);
 }
 
-const handlePlaylist = (playlistId, clickedPlaylistVideo, serviceListDispatch, isChecked) => {
+const handlePlaylist = (playlistId, clickedPlaylistVideo, serviceListDispatch, isChecked, setToastData) => {
     return !isChecked ? 
-        postVideoInPlaylist(playlistId, clickedPlaylistVideo, serviceListDispatch)
+        postVideoInPlaylist(playlistId, clickedPlaylistVideo, serviceListDispatch, setToastData)
         : 
-        deleteVideoFromPlaylist(playlistId, clickedPlaylistVideo._id, serviceListDispatch)  
+        deleteVideoFromPlaylist(playlistId, clickedPlaylistVideo._id, serviceListDispatch, setToastData)  
 }
 
 const playlistModalHandler = (video, setShowPlaylistModal, setClickedPlaylistVideo) => {
